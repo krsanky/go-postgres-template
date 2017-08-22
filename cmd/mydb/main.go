51b5be2481ad17ac0c0f1ed5a6297032f6631be1
paul wisehart <paul@oldcode.org>
2017-08-22 12:05:14 -0400

    add main program to tun Test()

---
+++ cmd/mydb/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/krsanky/go-postgres-template/db"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	db.Test()
+}
--- db/db.go
+++ db/db.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 
 	"github.com/krsanky/config"
+	"github.com/krsanky/lg"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func init() {
+	lg.Log.Printf("init pg db start ...")
 	var err error
 	DB, err = sql.Open("postgres",
 		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
@@ -22,16 +24,28 @@ func init() {
 	if err = DB.Ping(); err != nil {
 		panic(err)
 	}
+	Test()
 }
 
 func Test() {
-	//age := 21
-	//rows, err := db.Query("SELECT * FROM users WHERE age = $1", age)
+	var err error
+	if err = DB.Ping(); err != nil {
+		panic(err)
+	}
 
-	//rows, err := db.Query("SELECT * FROM account")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer rows.Close()
-
+	rows, err := DB.Query("SELECT username FROM account")
+	defer rows.Close()
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var username string
+		if err := rows.Scan(&username); err != nil {
+			panic(err)
+		}
+		lg.Log.Printf("username:%s", username)
+	}
+	if err := rows.Err(); err != nil {
+		panic(err)
+	}
 }
